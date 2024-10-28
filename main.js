68978286088832d2e4c4ea0560d8e0fb95dcb005
Dosyaan <dastanmatraimov79@gmail.com>
2024-10-28 17:57:11 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+// task - 1
+
+// const num = [1, 2, 3, 4, 5]
+// const sum = num.reduce((prev,item) => {
+//     return prev + item
+// })
+
+// console.log(sum)
+
+// task - 2
+
+// const num = [1, 2, 3, 4]
+// const mult = num.reduce((prev,item) =>{
+//     return prev * item
+// })
+
+// console.log(mult)
+
+// task - 3
+
+// const num = [10, 5, 8, 20, 3]
+// const max = num.reduce((prev, item) => {
+//     if (item > prev) {
+//         return item
+//     } else {
+//         return prev
+//     }
+// })
+
+// console.log(max);
+
+// task - 4
+
+// const num = [10, 5, 8, 20, 3]
+// const min = num.reduce((prev, item) => {
+//     if (item < prev) {
+//         return item
+//     } else {
+//         return prev
+//     }
+// })
+
+// console.log(min);
+
+// task - 5
+
+// const num = [1, 2, 3, 4, 5]
+// const string = num.reduce((prev,item) =>{
+//     return prev+item.toString()
+// })
+
+// console.log(string)
+
+// task - 6
+
+// const num = [{ value: 10 }, { value: 20 }, { value: 30 }]
+// const sum = num.reduce((prev,item) =>{
+//     return prev+item.value
+// }) 
+
+// console.log(sum)
+
+// task - 7
+
+// const word = ["да", "нет", "да", "нет", "да"]
+// const yes = word.reduce((prev,item) =>{
+//     return prev+(item === 'да')
+// },0)
+
+// console.log(yes)
+
+
+// task - 8
+
+// const num = [1, 2, 2, 3, 4, 4, 5]
+// const del = num.reduce((prev,item) =>{
+//     return prev 
+// })
+
+
+// task - 9
+
+// const num = [1, 2, 2, 3, 3, 3, 4]
+// const repaat = num.reduce((prev,item)=>{
+//     return prev
+// })
+
+// task - 10
+
+const num = [1, 2, 3, 4, 5]
+const reverse = num.reduce((item) =>{
+    return prev
+})
